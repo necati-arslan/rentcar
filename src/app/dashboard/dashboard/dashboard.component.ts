f24fea9d311ec79e5eedf281229ad254e1312054
necatia <necati.arslan2018@gmail.com>
2020-09-27 14:55:10 +0300

    second commit

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,28 +1,92 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import * as $ from "jquery";
-import { NgCircleProgressModule } from 'ng-circle-progress';
-import { DataService, Data,PopulationData } from 'src/app/service/data.service';
-import { DxChartModule } from 'devextreme-angular';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { NgCircleProgressModule } from "ng-circle-progress";
+import {
+  DataService,
+  Data,
+  PopulationData,
+} from "src/app/service/data.service";
+import { DxChartModule } from "devextreme-angular";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.css"],
 })
 export class DashboardComponent implements OnInit {
-
   dataSource: Data[];
   population: PopulationData[];
 
-  constructor(private dataService:DataService) {
-    this.dataSource=this.dataService.getDataSutun();
-    this.population=this.dataService.getDataPopulation();
-   }
- 
+  constructor(private dataService: DataService) {
+    this.dataSource = this.dataService.getDataSutun();
+    this.population = this.dataService.getDataPopulation();
+  }
+
   ngOnInit() {
+    this.tabsBarInit();
+  }
+
+  tabsBarInit() {
+    $("#tabs li a:not(:last)").addClass("inactive");
+    $(".container").hide();
+    $(".container:last").show();
+
+    $("#tabs li .textTab").click(function () {
+      console.log('.textTab')
+      var t = $(this).attr("id");
+      console.log(t)
+      if ($(this).hasClass("inactive")) {
+        //this is the start of our condition
+        $("#tabs li a").addClass("inactive");
+        $(this).removeClass("inactive");
+
+        $(".container").hide();
+        $("#" + t + "C").fadeIn("slow");
+      }
+    });
+
+    this.closeTab();
+    this.closeAllTabs()
+  }
+
+
+  closeTab(){
+    $("#tabs li .closeTab").unbind().click( function(e){
+      e.stopPropagation();
+      
+      const parent = $(this).parent();
+      const liElement = parent.parent();
+      const idTab = parent.attr('id'); 
+
+    liElement.remove();
+     $(`#${idTab}C`).remove();
+
+    if(!parent.hasClass("inactive")){
+      const lastElId=$("#tabs li a:last").attr('id')
+      $("#tabs li a:last").removeClass("inactive");
+      $("#" + lastElId + "C").fadeIn("slow");
+
+    }
+
+      console.log(parent.hasClass("inactive"));
+    });  
+  }
+
+  closeAllTabs(){
+    $("#tabs li .closeAll").unbind().click(function (e) {
+      e.stopPropagation();
+      
+      $("#tabs li:not(:first)").remove();
+      $(".container").remove();
+
+    })
+
     
   }
 
+
 }
+
+
--- src/app/navigation/sidenav/sidenav.component.ts
+++ src/app/navigation/sidenav/sidenav.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, AfterViewInit, ElementRef, Renderer2 } from "@angular/core";
-import { NavigateService } from "../navigate-service.service";
+import { Component, OnInit,  } from "@angular/core";
+
 import * as $ from "jquery";
 
 @Component({
@@ -8,24 +8,14 @@ import * as $ from "jquery";
   styleUrls: ["./sidenav.component.css"],
 })
 export class SidenavComponent implements OnInit {
-  sideNavState$;
-  isOpen = false;
-
-  globalListenFunc: Function;
-  sidemenu: Function;
+  
   constructor(
-    private navigateService: NavigateService,
-    private elementRef: ElementRef,
-    private renderer: Renderer2
+    
   ) {}
 
   ngOnInit() {
     this.dropdownMenu();
-    this.navigateService.stateNavEmit(false);
-    this.sideNavState$ = this.navigateService.isOpen.subscribe((value) => {
-      console.log(value);
-      this.openNav();
-    });
+   
   }
 
   dropdownMenu() {
@@ -91,38 +81,9 @@ export class SidenavComponent implements OnInit {
     }
   }
 
-  ngOnDestroy() {
-    this.sideNavState$.unsubscribe();
-    this.globalListenFunc();
-    this.sidemenu();
-  }
 
-  ngAfterViewInit() {
-    this.sidemenu = this.elementRef.nativeElement
-      .querySelector("#sideMenu")
-      .addEventListener("click", (e) => {
-        e.stopPropagation();
-      });
 
-    this.globalListenFunc = this.renderer.listen("document", "click", (event) => {
-    // Do something with 'event'
-    console.log("dddd");
-    this.closeNav();
-});
-  }
 
-  openNav() {
-    if (!this.isOpen) {
-      document.getElementById("sideMenu").style.width = "80%";
-    } else {
-      document.getElementById("sideMenu").style.width = "0";
-    }
-    this.isOpen = !this.isOpen;
-  }
 
-  closeNav() {
-    if (!this.isOpen) return;
-    document.getElementById("sideMenu").style.width = "0";
-    this.isOpen = false;
-  }
+
 }
